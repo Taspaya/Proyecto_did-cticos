9e18a029f2765d5765fc7859f8e0df6d0c5a9a9d
sergitim <git config --global user.email sergitim@gmail.com>
2020-10-19 21:58:34 +0200

    Title Screen and small fixes
    * Added a Title Screen with Play, Instructions, Settings and  Quit. All buttons work, though the Instructions and the Settings Screen are still empty until we decide what to put in them.
    
    * Changed Project Settings from 2D Project to 3D project
    
    * Deleted empty Scene and relocated Prototype scene to the Scenes folder.

---
+++ Assets/Scripts/TitleButtons.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleButtons : MonoBehaviour
+{
+
+    public bool instructions = false;
+    public bool settings = false;
+    public GameObject MainButtons;
+    public GameObject instructionScreen;
+    public GameObject settingsScreen;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtInstructions()
+    {
+        instructions = true;
+        MainButtons.SetActive(false);
+        instructionScreen.SetActive(true);
+        
+    }
+
+    public void ButtSettings()
+    {
+        settings = true;
+        MainButtons.SetActive(false);
+        settingsScreen.SetActive(true);
+    }
+
+
+    public void ButtQuit()
+    {
+        Application.Quit();
+    }
+
+    public void ButtReturn()
+    {
+        instructionScreen.SetActive(false);
+        settingsScreen.SetActive(false);
+        MainButtons.SetActive(true);
+        instructions = false;
+        settings = false;
+    }
+
+
+}
