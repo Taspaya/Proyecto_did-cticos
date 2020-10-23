d0f02d2db894378ed45d08e8bcaa7deb1ab42d88
ausiasxp <72753790+ausiasxp@users.noreply.github.com>
2020-10-23 02:10:12 +0200

    Add files via upload
    Added ScoreManager to manage the infected people and Virus Life score.
    
    Added example script to modify the variables.

---
+++ Assets/Scripts/DemoScore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScore : MonoBehaviour
+{
+    ScoreManager scoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+        
+        scoreManager.ScorePLife = PlayerPrefs.GetInt("ScorePLife", 0);
+        scoreManager.ScoreVLife = PlayerPrefs.GetInt("ScoreVLife", 0);
+
+        scoreManager.ScoreVLife = 100;
+        scoreManager.ScorePLife = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            scoreManager.ScorePLife = scoreManager.ScorePLife + 1;
+        }
+        if (scoreManager.ScoreVLife >= 1) { 
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+            scoreManager.ScoreVLife = scoreManager.ScoreVLife - 1;
+            } 
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] Text TextScoreUIP;
+    [SerializeField] Text TextScoreUIV;
+    private int scoreVirus;
+    private int scorePerson;
+    public int ScoreVLife
+    {
+        get { return scoreVirus; }
+        set
+        {
+            scoreVirus = value;
+
+            TextScoreUIV.text = ScoreVLife.ToString();
+
+            PlayerPrefs.SetInt("ScoreVLife", ScoreVLife);
+        }
+    }
+
+    public int ScorePLife
+    {
+        get { return scorePerson; }
+        set
+        {
+            scorePerson = value;
+
+            TextScoreUIP.text = ScorePLife.ToString();
+
+            PlayerPrefs.SetInt("ScorePLife", ScorePLife);
+        }
+    }
+}
