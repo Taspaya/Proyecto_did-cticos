48d1d11bc2ee0fb0112e6a77c9b51c36c5b202a7
sergitim <git config --global user.email sergitim@gmail.com>
2020-12-04 10:24:16 +0100

    Moving Dummy Obstacles
    Testing obstacles in motion

--- Assets/Scripts/Virus/Obstacle.cs
+++ Assets/Scripts/Virus/Obstacle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Obstacle : MonoBehaviour
 {
 
@@ -11,12 +12,16 @@ public class Obstacle : MonoBehaviour
 
     public ScoreManager scoreManager;
     public CameraControl cameraControl;
+    public int myPosition; // It will progress between 1, 2 3 and 4, then return to 1. When it is 1, it will move up. When it is 2 it will move left. When it is 3, it will move down. When it is 4, it will move right.
+
+
     // Start is called before the first frame update
     void Start()
     {
         scoreManager = FindObjectOfType<ScoreManager>();
         cameraControl = FindObjectOfType<CameraControl>();
 
+        
         //damage = 10;
         //knockback = 1;
         //impactRange = 1;
@@ -25,7 +30,7 @@ public class Obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -41,6 +46,40 @@ public class Obstacle : MonoBehaviour
         }
 
         //Destroy(this.gameObject);
+    }
+
+    public void movement()
+    {
+
+        //Debug.Log("Myposition: " + myPosition + "....." + transform.position);
+        if (myPosition == 1)
+        {
+            transform.Translate(0, 1, 0);
+            if (transform.position.y > 10) { myPosition = 2; }
+            return;
+        }
+
+        if (myPosition == 2)
+        {
+            transform.Translate(1, 0, 0);
+            if (transform.position.x > 10) { myPosition = 3; }
+            
+            return;
+        }
+
+        if (myPosition == 3)
+        {
+            transform.Translate(0, -1, 0);
+            if (transform.position.y < 0) { myPosition = 4; }
+            return;
+        }
+        if (myPosition == 4)
+        {
+            transform.Translate(-1, 0, 0);
+            if (transform.position.x < 0) { myPosition = 1; }
+            return;
+        }
+
     }
 
 }
