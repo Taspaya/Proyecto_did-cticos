0f4b468b655a07aefcb863758f0a697f6223a49e
eduedy01 <eduardnavarro21@gmail.com>
2021-01-04 12:18:52 +0100

    NPC
    -Shader de NPC que parpadea en rojo acabado. (Indica zona donde tiene que colisionar el jugador para contagiar)
    - Mampara más oscura

--- Assets/Materials/NPC/TintController.cs
+++ Assets/Materials/NPC/TintController.cs
@@ -9,12 +9,41 @@ public class TintController : MonoBehaviour
     public float tintAmount;
     public Material tintMat;
     public float blinkSpeed = 5f;
+    public bool rojoVa = true; // Controla si se aumenta o disminuye el tinte rojo del modelo.
 
     // Update is called once per frame
     void Update()
     {
-        tintAmount = Mathf.Lerp(tintAmount, 0, Time.deltaTime * blinkSpeed);
-        tintAmount = Mathf.Clamp(tintAmount, 0, 1);
-        tintMat.SetFloat("_TintAmount", tintAmount);
+        if (!rojoVa)
+        {
+            //Debug.Log("RojoVa es " + rojoVa);
+
+            if (tintAmount >= 0.9f)
+            {
+                rojoVa = true;
+            }
+            else
+            {
+                tintAmount = Mathf.Lerp(tintAmount, 1, Time.deltaTime * blinkSpeed);
+                tintAmount = Mathf.Clamp(tintAmount, 0, 1); 
+            }
+        }
+        else
+        {
+            //Debug.Log("RojoVa es " + rojoVa);
+            if (tintAmount <= 0.1f)
+            {
+                rojoVa = false;
+            }
+            else
+            {
+                tintAmount = Mathf.Lerp(tintAmount, 0, Time.deltaTime * blinkSpeed);
+                tintAmount = Mathf.Clamp(tintAmount, 0, 1); 
+            }
+
+        }
+
+        tintMat.SetFloat("_TintAmount", tintAmount); 
+        //Debug.Log("TintAmount es " + tintAmount);
     }
 }
