fbfa0e7f14732efc72635692d65548c3263b3912
eduedy01 <eduardnavarro21@gmail.com>
2021-01-07 13:20:49 +0100

    Obstáculos
    Movimento de mamapara y posicionamiento.
    Colliders Metro

--- Assets/Scripts/Virus/Obstacle.cs
+++ Assets/Scripts/Virus/Obstacle.cs
@@ -14,7 +14,7 @@ public class Obstacle : MonoBehaviour
     public ScoreManager scoreManager;
     public CameraControl cameraControl;
     public int myPosition; // It will progress between 1, 2 3 and 4, then return to 1. When it is 1, it will move up. When it is 2 it will move left. When it is 3, it will move down. When it is 4, it will move right.
-
+    public float speedControl;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +22,7 @@ public class Obstacle : MonoBehaviour
         hitSound = GetComponent<AudioSource>();
         scoreManager = FindObjectOfType<ScoreManager>();
         cameraControl = FindObjectOfType<CameraControl>();
+        
 
         
         //damage = 10;
@@ -37,6 +38,7 @@ public class Obstacle : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("COLISON CON OBSTACULO " + this.gameObject.name);
         hitSound.Play();
         scoreManager.ScoreVLife -= damage;
         cameraControl.speed = -50; //A method in the CameraControl sript will reset the speed back to normal after a few seconds have passed since the collision.
@@ -54,32 +56,32 @@ public class Obstacle : MonoBehaviour
 
     public void movement()
     {
-
-        //Debug.Log("Myposition: " + myPosition + "....." + transform.position);
+        
+        Debug.Log("Myposition: " + myPosition + "....." + transform.position);
         if (myPosition == 1)
         {
-            transform.Translate(0, 1, 0);
-            if (transform.position.y > 10) { myPosition = 2; }
+            transform.Translate(0, 1 * speedControl, 0);
+            if (transform.position.y > 20) { myPosition = 2; }
             return;
         }
 
         if (myPosition == 2)
         {
-            transform.Translate(1, 0, 0);
-            if (transform.position.x > 10) { myPosition = 3; }
+            transform.Translate(1 * speedControl, 0, 0);
+            if (transform.position.x > 20) { myPosition = 3; }
             
             return;
         }
 
         if (myPosition == 3)
         {
-            transform.Translate(0, -1, 0);
+            transform.Translate(0, -1 * speedControl, 0);
             if (transform.position.y < 0) { myPosition = 4; }
             return;
         }
         if (myPosition == 4)
         {
-            transform.Translate(-1, 0, 0);
+            transform.Translate(-1 * speedControl, 0, 0);
             if (transform.position.x < 0) { myPosition = 1; }
             return;
         }
